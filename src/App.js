7b25b6bbdaf5b9cbda8c096b0fd3ecad5c318f99
gtildis <gretil_15@hotmail.com>
2023-06-26 20:12:47 +0200

    asd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import "./App.css";
+import { Routes, Route } from "react-router-dom"; // <== IMPORT
+
+import Navbar from "./components/Navbar"; // <== IMPORT
+import HomePage from "./pages/Homepage"; // <== IMPORT
+import ProjectListPage from "./pages/ProjectListPage"; //  <== IMPORT
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			{/* Below: ADD <Navbar>, <Routes> & <Route> */}
+			<Navbar />
+
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/projects" element={<ProjectListPage />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
